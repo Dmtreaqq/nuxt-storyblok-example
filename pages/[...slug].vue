d4ed44e27e15ca8605edbec6d6055c7afdffcc31
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-10-22 13:15:55 +0300

    feat: update to async

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -5,7 +5,7 @@
 <script setup>
     const { slug } = useRoute().params
 
-    const story = await useStoryblok(
+    const story = await useAsyncStoryblok(
         slug && slug.length > 0 ? slug.join('/') : 'home', { version: 'draft' }
     )
 </script>
